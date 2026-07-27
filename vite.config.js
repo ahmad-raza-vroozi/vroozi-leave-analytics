7a53cb6df1a320ac61f82ee6b9564880a3f60bcc
minalkashif1 <161220184+minalkashif1@users.noreply.github.com>
2026-07-27 14:21:32 +0500

    Set base path for Vite config
    Updated base path in Vite configuration for deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace <your-repo-name> below with the exact name of this GitHub repo,
+  // e.g. if your repo is github.com/minalkashif1/hr-leave-dashboard,
+  // this should be base: '/hr-leave-dashboard/'
+  base: '/<vroozi-leave-analytics>/',
 })
